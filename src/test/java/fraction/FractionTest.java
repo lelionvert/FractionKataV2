7150f87750b5cc92245a8894b77eb6ec65d06f00
promo 13 <abderrezak.khif@gmail.com>
2020-03-03 14:55:59 +0100

    Initialize project

---
+++ src/test/java/fraction/FractionTest.java
@@ -0,0 +1,13 @@
+package fraction;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FractionTest {
+
+    @Test
+    public void testSetup() {
+        assertThat(true).isTrue();
+    }
+}
