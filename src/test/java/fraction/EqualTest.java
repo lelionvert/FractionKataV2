76e4b981722c1e3d2f6828e506b260ad8b00a473
promo 13 <abderrezak.khif@gmail.com>
2020-03-05 15:24:51 +0100

    refactor EqualTest

--- src/test/java/fraction/EqualTest.java
+++ src/test/java/fraction/EqualTest.java
@@ -2,42 +2,44 @@ package fraction;
 
 import org.junit.jupiter.api.Test;
 
+import static fraction.Fraction.fraction;
+import static fraction.Fraction.integerFraction;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EqualTest {
     @Test
     public void equalityOfFractions(){
-        Fraction fraction = new Fraction(1);
-        Fraction fractionWithSameValue = new Fraction(1);
+        Fraction fraction = integerFraction(1);
+        Fraction fractionWithSameValue = integerFraction(1);
         assertThat(fraction).isEqualTo(fractionWithSameValue);
     }
 
     @Test
     public void inEqualityOfTwoFraction(){
-        Fraction fraction = new Fraction(1);
-        Fraction anotherFraction = new Fraction(2);
+        Fraction fraction = integerFraction(1);
+        Fraction anotherFraction = integerFraction(2);
         assertThat(fraction).isNotEqualTo(anotherFraction);
 
     }
 
     @Test
     public void inequalityOfAnythingNotAFraction(){
-        Fraction fraction = new Fraction(1);
+        Fraction fraction = integerFraction(1);
         Integer otherObject = 1;
         assertThat(fraction).isNotEqualTo(otherObject);
     }
 
     @Test
     public void inequalityOfTwoInstancesOfDifferentConstructors(){
-        Fraction fractionWithOneArgument = new Fraction(1);
-        Fraction fractionWithTwoArguments = new Fraction(1,2);
+        Fraction fractionWithOneArgument = integerFraction(1);
+        Fraction fractionWithTwoArguments = fraction(1, 2);
         assertThat(fractionWithOneArgument).isNotEqualTo(fractionWithTwoArguments);
     }
 
     @Test
     public void equalityOfOneHalfAndOneHalf(){
-        Fraction firstHalf = new Fraction(1,2);
-        Fraction secondHalf = new Fraction(1,2);
+        Fraction firstHalf = fraction(1, 2);
+        Fraction secondHalf = fraction(1, 2);
         assertThat(firstHalf).isEqualTo(secondHalf);
     }
 
