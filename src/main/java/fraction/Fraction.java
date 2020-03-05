47ef516e0ca299962f2bf08b7a556428d202acd8
promo 13 <abderrezak.khif@gmail.com>
2020-03-05 11:48:01 +0100

    refactor equals method step 2

--- src/main/java/fraction/Fraction.java
+++ src/main/java/fraction/Fraction.java
@@ -16,30 +16,20 @@ public class Fraction {
     }
 
     public Fraction add(Fraction other) {
-        if( this.denominator != null && this.denominator == 3){
+        if (this.denominator != null && this.denominator == 3) {
             return new Fraction(2, 3);
         }
-         return new Fraction(this.value + other.value);
+        return new Fraction(this.value + other.value);
     }
 
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Fraction) {
             Fraction other = (Fraction) obj;
-            if (other.denominator != null) {
-                if (this.denominator != null) {
-                    return other.value.equals(this.value)
-                            && other.denominator.equals(this.denominator);
-                }
-                if (other.denominator == 2) {
-                    return false;
-                }
-            }else{
-                if(this.denominator != null){
-                    return false;
-                }
-                return other.value.equals(this.value);
-            }
+
+            return other.value.equals(this.value)
+                    && other.denominator.equals(this.denominator);
+
         }
         return false;
     }
